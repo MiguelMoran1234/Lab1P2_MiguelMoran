0cd900a1a6f4b455552681578340663eee2a4394
Hdbygdla <131492426+MiguelMoran1234@users.noreply.github.com>
2025-04-25 13:22:37 -0600

    Se creo el menu

---
+++ src/lab1p2_miguelmoran/Lab1P2_MiguelMoran.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1p2_miguelmoran;
+
+import java.util.Scanner;
+
+public class Lab1P2_MiguelMoran {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean seguir = true;
+        while(seguir){
+            System.out.println("---------Menu---------");
+            System.out.println("1. Jugar Numberly");
+            System.out.println("2. Verificar Palindromo");
+            System.out.println("3. Salir");
+            System.out.print("Ingrese una opcion: ");
+            int opcion = sc.nextInt();
+            switch(opcion){
+                case 1 -> {
+                    
+                }
+            }
+        }
+    }
+    
+}
